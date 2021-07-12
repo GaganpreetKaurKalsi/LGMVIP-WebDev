734f5beda5d25148b057d511b31490259e8922cf
Gaganpreet Kaur Kalsi <54144759+GaganpreetKaurKalsi@users.noreply.github.com>
2021-07-12 13:33:29 +0530

    fixed hamburger menu

--- Task1-SPWebsite/script.js
+++ Task1-SPWebsite/script.js
@@ -1,6 +1,7 @@
 // ----------------------------------------------Final Scripts-----------------------------------------------------
 
 // --------------Scripts for navbar-responsive---------------------
+let menuOpen = false
 
 function showElement(query) {
   document.querySelector(query).style.visibility = "visible";
@@ -13,13 +14,39 @@ function hideElement(query) {
 function openMenu() {
   showElement(".nav-container ul");
   showElement(".nav-container .close");
+  menuOpen = true;
 }
 function closeMenu() {
   hideElement(".nav-container ul");
   hideElement(".nav-container .close");
+  menuOpen = false;
 }
-document.querySelector(".hamburger").addEventListener("click", openMenu)
-document.querySelector(".close").addEventListener("click", closeMenu);
+// document.querySelector(".hamburger").addEventListener("click", openMenu)
+// document.querySelector(".close").addEventListener("click", closeMenu);
+
+
+function toggleHam(e) {
+  const ul = document.querySelector(".nav-container ul")
+  const a = document.querySelectorAll(".nav-container ul li a");
+  const hamburger = document.querySelector(".hamburger");
+  if (!menuOpen && hamburger.contains(e.target)) {
+    openMenu()
+  }
+  else if (menuOpen && !ul.contains(e.target)) {
+    closeMenu()
+  }
+  else{
+    return null
+  }
+}
+window.addEventListener("click",toggleHam)
+
+document.querySelectorAll('.nav-container ul li a').forEach((a) => {
+  a.addEventListener("click", () => {
+    if(window.innerWidth<=768)
+      closeMenu()
+  })
+})
 
 // ---------------------------------------------Scripts for Main-Caroussel----------------------------------
 
